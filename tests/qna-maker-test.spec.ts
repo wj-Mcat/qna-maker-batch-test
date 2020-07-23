dd81d49e438bf7710c9358e541c59e5447d0ed92
wjmcat <1435130236@qq.com>
2020-07-23 17:26:19 +0800

    update test script

--- tests/qna-maker-test.spec.ts
+++ tests/qna-maker-test.spec.ts
@@ -1,27 +1,42 @@
 #!/usr/bin/env ts-node
 
 import test  from 'tstest'
-const superagent = require('superagent')
+import axios from 'axios'
 
-let ENDPOINT = process.env.QNA_ENDPOINT
-let KNOLEDGE_BASE_TOKEN = process.env.QNA_KNOWLEDGE_BASE_TOKEN
+const ENDPOINT = process.env.QNA_ENDPOINT
+const KNOLEDGE_BASE_TOKEN = process.env.QNA_KNOWLEDGE_BASE_TOKEN
 
-let agentPromise = superagent.
-        post(`https://wechatyqnachinese.azurewebsites.net/qnamaker/knowledgebases/${KNOLEDGE_BASE_TOKEN}/generateAnswer`).
-        set('Authorization', `EndpointKey ${ENDPOINT}`).
-        set('Content-Type', 'application/json')
+const url = `https://wechatyqnachinese.azurewebsites.net/qnamaker/knowledgebases/${KNOLEDGE_BASE_TOKEN}/generateAnswer`;
+const headers = {
+    'Authorization': `EndpointKey ${ENDPOINT}`,
+    'Content-Type': 'application/json',
+}
+
+export interface TestData{
+    question: string,
+    qna_id: number
+}
+
+async function read_data(): Promise<Array<TestData>>{
+    // load test_data from remote_url/local_file/db
+    const test_data = [{
+        question: "python-wechaty 怎么跑不起来啊",
+        qna_id: 77,
+    }, {
+        question: "Token 有什么用？",
+        qna_id: 40,
+    }]
+    return test_data
+}
 
 test('qna-maker ', async t => {
-    // load test_data
-    let test_data = [
-        {"question": "python 怎么运行不起来", "qna_id": 77}, {"question": "token如何申请", "qna_id": 43}]
-
-    for (let index = 0; index < test_data.length; index++) {
-        const item = test_data[index];
-        let res = await agentPromise.send({question: item['question']})
-        let answers = res.body.answers
+    let test_data = await read_data()
+    for (const item of test_data) {
+        const res = await axios.post(url, { question: item.question }, { headers })
+        const answers: Array<object> = res.data.answers
+        console.log(answers)
         // make sure the specific answer in response
-        console.log(item)
-        console.log(answers)    
+        const target_answer = answers.filter(x => x['id'] == item.qna_id)
+        t.assert(target_answer.length, 1)
     }
 })
